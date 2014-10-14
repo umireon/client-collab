c2b8e9abf38378961cda980bb879addfc223c1ec
Kaito Udagawa <umireon@gmail.com>
2014-10-14 12:35:52 +0900

    Optimize Canvas Operations and Create Classes

--- 8psk_cnr_coding.js
+++ 8psk_cnr_coding.js
@@ -1,103 +1,246 @@
-function encode(sym) {
-    var nat = document.getElementById("nat");
-    if (nat.checked) {
-        return sym;
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Coding = (function () {
+    function Coding(encoder) {
+        this._enc = encoder;
+        this._dec = [];
+        var loop = encoder.length;
+        for (var i = 0; i < loop; i++) {
+            this._dec[this._enc[i]] = i;
+        }
     }
-    else {
-        return [0, 1, 3, 2, 6, 7, 5, 4][sym];
+    Coding.prototype.encode = function (index) {
+        return this._enc[index];
+    };
+    Coding.prototype.decode = function (code) {
+        return this._dec[code];
+    };
+    return Coding;
+})();
+;
+var GrayCoding = (function (_super) {
+    __extends(GrayCoding, _super);
+    function GrayCoding(bitWidth) {
+        var encoder = [];
+        var loop = (1 << bitWidth);
+        for (var i = 0; i < loop; i++) {
+            encoder[i] = i ^ (i >> 1);
+        }
+        _super.call(this, encoder);
     }
-}
-function decode(code) {
-    var nat = document.getElementById("nat");
-    if (nat.checked) {
+    return GrayCoding;
+})(Coding);
+;
+var NaturalBinaryCoding = (function (_super) {
+    __extends(NaturalBinaryCoding, _super);
+    function NaturalBinaryCoding() {
+        _super.call(this, []);
+    }
+    NaturalBinaryCoding.prototype.encode = function (index) {
+        return index;
+    };
+    NaturalBinaryCoding.prototype.decode = function (code) {
         return code;
+    };
+    return NaturalBinaryCoding;
+})(Coding);
+;
+var Complex = (function () {
+    function Complex(r, i) {
+        this.real = r;
+        this.imag = i;
     }
-    else {
-        return [0, 1, 3, 2, 7, 6, 4, 5][code];
+    Complex.polar = function (r, theta) {
+        return new Complex(r * Math.cos(theta), r * Math.sin(theta));
+    };
+    /* destructive */
+    Complex.prototype.add = function (other) {
+        this.real += other.real;
+        this.imag += other.imag;
+        return this;
+    };
+    /* destructive */
+    Complex.prototype.sub = function (other) {
+        this.real -= other.real;
+        this.imag -= other.imag;
+        return this;
+    };
+    /* destructive */
+    Complex.prototype.mul = function (other) {
+        var treal = this.real, timag = this.imag;
+        var oreal = other.real, oimag = other.imag;
+        this.real = treal * oreal - timag * oimag;
+        this.imag = treal * oimag + timag * oreal;
+        return this;
+    };
+    return Complex;
+})();
+var PskModulator = (function () {
+    function PskModulator(m) {
+        this._unit = 2 * Math.PI / m;
     }
-}
-function do_qpsk() {
-    var c = document.getElementById("c");
-    var ctx = c.getContext("2d");
-    var mag = 100;
-    ctx.clearRect(0, 0, 800, 800);
-    var sigma_input = document.getElementById("sigma");
-    var sigma = Math.sqrt(0.5 / Math.pow(10, Number(sigma_input.value) / 10));
-    //ctx.globalAlpha = 0.6;
-    var err = 0;
-    for (var i = 0; i < 10000; i++) {
-        var u0 = Math.random(), u1 = Math.random(), u2 = Math.random();
-        var tx_code = Math.floor(u0 * 8);
-        var tx_sym = decode(tx_code);
-        var si = Math.cos(Math.PI * tx_sym / 4);
-        var sq = Math.sin(Math.PI * tx_sym / 4);
-        var sig_sq_mlu1_m2 = sigma * Math.sqrt(-2 * Math.log(u1));
-        var piu2_2 = 2 * Math.PI * u2;
-        var ni = sig_sq_mlu1_m2 * Math.cos(piu2_2);
-        var nq = sig_sq_mlu1_m2 * Math.sin(piu2_2);
-        var zi = si + ni;
-        var zq = sq + nq;
-        var ri = Math.cos(Math.PI / 8);
-        var rq = Math.sin(Math.PI / 8);
-        var di = zi * ri - zq * rq, dq = zi * rq + zq * ri;
-        var rri = Math.cos(Math.PI / 4);
-        var rrq = Math.sin(Math.PI / 4);
-        var rrri = Math.cos(-Math.PI / 4);
-        var rrrq = Math.sin(-Math.PI / 4);
-        var cwi = di * rri - dq * rrq, cwq = di * rrq + dq * rri;
-        var rx_sym;
-        if (di > 0 && dq > 0) {
-            if (cwi > 0) {
-                ctx.fillStyle = "darkred";
-                rx_sym = 0;
-            }
-            else {
-                ctx.fillStyle = "red";
-                rx_sym = 1;
-            }
+    PskModulator.prototype.modulate = function (sym) {
+        return Complex.polar(1, sym * this._unit);
+    };
+    return PskModulator;
+})();
+var Psk8Demodulator = (function () {
+    function Psk8Demodulator() {
+        this.rotate4 = Complex.polar(1, Math.PI / 4);
+        this.rotate8 = Complex.polar(1, Math.PI / 8);
+    }
+    Psk8Demodulator.prototype.demodulate = function (vect) {
+        vect.mul(this.rotate8);
+        var quad_i = vect.real, quad_q = vect.imag;
+        vect.mul(this.rotate4);
+        var half_i = vect.real, half_q = vect.imag;
+        if (quad_i > 0 && quad_q > 0) {
+            return half_i > 0 ? 0 : 1;
         }
-        else if (di > 0 && dq < 0) {
-            if (cwq > 0) {
-                ctx.fillStyle = "darkblue";
-                rx_sym = 7;
-            }
-            else {
-                ctx.fillStyle = "blue";
-                rx_sym = 6;
-            }
+        else if (quad_i > 0 && quad_q < 0) {
+            return half_q > 0 ? 7 : 6;
         }
-        else if (di < 0 && dq > 0) {
-            if (cwq > 0) {
-                ctx.fillStyle = "darkgreen";
-                rx_sym = 2;
-            }
-            else {
-                ctx.fillStyle = "lightgreen";
-                rx_sym = 3;
-            }
+        else if (quad_i < 0 && quad_q > 0) {
+            return half_q > 0 ? 2 : 3;
         }
         else {
-            if (cwi > 0) {
-                ctx.fillStyle = "gray";
-                rx_sym = 5;
+            return half_i > 0 ? 5 : 4;
+        }
+    };
+    return Psk8Demodulator;
+})();
+var AwgnGenerator = (function () {
+    function AwgnGenerator(sigma) {
+        this._pi2 = Math.PI * 2;
+        this._sigma = sigma;
+    }
+    AwgnGenerator.prototype.generate = function (u1, u2) {
+        var a = this._sigma * Math.sqrt(-2 * Math.log(u1));
+        var t = this._pi2 * u2;
+        return new Complex(a * Math.cos(t), a * Math.sin(t));
+    };
+    return AwgnGenerator;
+})();
+var Symbol = (function () {
+    function Symbol() {
+    }
+    return Symbol;
+})();
+var CanvasView = (function () {
+    function CanvasView() {
+        this._canvas = document.getElementById('c');
+        this._cctx = this._canvas.getContext('2d');
+        this._shadow = document.createElement('canvas');
+        this._shadow.width = this._canvas.width;
+        this._shadow.height = this._canvas.height;
+        this.context = this._shadow.getContext('2d');
+    }
+    CanvasView.prototype.show = function () {
+        this._cctx.drawImage(this._shadow, 0, 0);
+    };
+    return CanvasView;
+})();
+var SimulatorFacade = (function () {
+    function SimulatorFacade() {
+        this.canvas = new CanvasView;
+        this.coding = new GrayCoding(3);
+        this.awgn = new AwgnGenerator(0.223);
+        this.modulator = new PskModulator(8);
+        this.demodulator = new Psk8Demodulator();
+        this.listenOnCoding();
+        this.listenOnCnr();
+    }
+    SimulatorFacade.prototype.listenOnCoding = function () {
+        var that = this;
+        var inputCoding = document.getElementById('nat');
+        inputCoding.addEventListener('change', function () {
+            if (inputCoding.checked) {
+                that.coding = new NaturalBinaryCoding;
             }
             else {
-                ctx.fillStyle = "black";
-                rx_sym = 4;
+                that.coding = new GrayCoding(3);
+            }
+        });
+    };
+    SimulatorFacade.prototype.listenOnCnr = function () {
+        var that = this;
+        var inputCnr = document.getElementById('sigma');
+        inputCnr.addEventListener('change', function () {
+            var cnrdb = Number(inputCnr.value) / 10;
+            var cnr = Math.pow(10, cnrdb);
+            var sigma = Math.sqrt(0.5 / cnr);
+            that.awgn = new AwgnGenerator(sigma);
+        });
+    };
+    return SimulatorFacade;
+})();
+var Main = (function () {
+    function Main() {
+        this._facade = new SimulatorFacade;
+    }
+    Main.prototype.animate_psk8 = function () {
+        var that = this;
+        (function loop() {
+            requestAnimationFrame(function () {
+                that.do_psk8();
+            });
+            setTimeout(loop, 1000 / 15);
+        })();
+    };
+    Main.prototype.do_psk8 = function () {
+        var mag = 100;
+        var bufsize = 500;
+        var ctx = this._facade.canvas.context;
+        var coding = this._facade.coding;
+        var mod = this._facade.modulator;
+        var demod = this._facade.demodulator;
+        var awgn = this._facade.awgn;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 400, 400);
+        var err = 0;
+        var symbols = new Array(bufsize);
+        for (var i = 0; i < 20; i++) {
+            for (var j = 0; j < bufsize; j++) {
+                symbols[j] = new Symbol();
+                var symbol = symbols[j];
+                var u0 = Math.random();
+                var u1 = Math.random();
+                var u2 = Math.random();
+                symbol.tx_code = Math.floor(u0 * 8);
+                symbol.tx_sym = coding.decode(symbol.tx_code);
+                symbol.vect = mod.modulate(symbol.tx_sym);
+                symbol.vect.add(awgn.generate(u1, u2));
+                symbol.rx_sym = demod.demodulate(symbol.vect);
+                symbol.rx_code = coding.encode(symbol.rx_sym);
+            }
+            for (var k = 0; k < 8; k++) {
+                ctx.fillStyle = ['darkred', 'red', 'darkred', 'darkgreen', 'lightgreen', 'black', 'gray', 'blue', 'darkblue'][k];
+                for (var j = 0; j < bufsize; j++) {
+                    var symbol = symbols[j];
+                    if (symbol.rx_code != k)
+                        continue;
+                    if (symbol.tx_code == symbol.rx_code) {
+                        var x = 150 - Math.round(mag * symbol.vect.real);
+                        var y = 200 - Math.round(mag * symbol.vect.imag);
+                        ctx.fillRect(x, y, 1, 1);
+                    }
+                    else {
+                        var d = symbol.rx_code ^ symbol.tx_code;
+                        var dist = ((d >> 2) & 1) + ((d >> 1) & 1) + (d & 1);
+                        err += dist;
+                        var x = 150 - Math.round(mag * symbol.vect.real);
+                        var y = 200 - Math.round(mag * symbol.vect.imag);
+                        ctx.fillRect(x, y, 3 + 3 * dist, 3 + 3 * dist);
+                    }
+                }
             }
         }
-        var rx_code = encode(rx_sym);
-        if (tx_sym == rx_sym) {
-            ctx.fillRect(150 + mag * zi, 200 + mag * zq, 1, 1);
-        }
-        else {
-            var d = rx_code ^ tx_code;
-            var dist = ((d >> 2) & 1) + ((d >> 1) & 1) + (d & 1);
-            err += dist;
-            ctx.fillRect(150 + mag * zi, 200 + mag * zq, 3 + 3 * dist, 3 + 3 * dist);
-        }
-    }
-    var pb = document.getElementById("pb");
-    pb.value = (Math.LN10 * Math.log(err / 30000.0)).toPrecision(20);
-}
-setInterval(do_qpsk, 50);
+        this._facade.canvas.show();
+    };
+    return Main;
+})();
+new Main().animate_psk8();
